aae897c7a4a6b386913cccd493fb33bd39330db6
Chris Bremer <cbremer4@gmail.com>
2025-06-22 18:34:35 -0500

    Added Client

---
+++ JustFSharpThings.Client/Program.fs
@@ -0,0 +1,15 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+
+open System.Net.Http
+
+module Client =
+    [<Literal>]
+    let BaseAddress = "http://localhost:5000/api/"
+    let client() =
+        new HttpClient(BaseAddress = System.Uri(BaseAddress))
+
+
+[<EntryPoint>]
+let main argv =
+    
