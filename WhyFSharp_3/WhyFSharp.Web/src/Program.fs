d08c4e8eafcdc7fdbb6958b0cecf82d2a5944323
Chris Bremer <cbremer4@gmail.com>
2025-07-31 20:53:09 -0500

    Nothing really

--- WhyFSharp_3/WhyFSharp.Web/src/Program.fs
+++ WhyFSharp_3/WhyFSharp.Web/src/Program.fs
@@ -20,41 +20,41 @@ let update msg model =
         {model with Text = text}, Cmd.none
 
 let render model dispatch =
-        
+            
     Html.div [
+        prop.key "MainContainer"
         prop.className [
             "flex"; "flex-col"; "items-center"; "justify-center"; "gap-4"; "my-16";
         ]
         prop.children [
             Html.div [
-                prop.className "flex"
+                prop.key "InputContainer"
+                prop.className ["w-xl"; ]
                 prop.children [
                     Html.textarea [
-                        prop.className ["block"; "p-2.5"; "w-full"; "text-sm"; "text-gray-900"; "bg-gray-50"; "rounded-lg"; "border"; "border-gray-300"; "focus:ring-blue-500"; "focus:border-blue-500"; "dark:bg-gray-700"; "dark:border-gray-600"; "dark:placeholder-gray-400"; "dark:text-white"; "dark:focus:ring-blue-500"; "dark:focus:border-blue-500"]
+                        prop.className [ "resize"; "block"; "p-2.5"; "w-full"; "text-sm"; "text-gray-900"; "bg-gray-50"; "rounded-lg"; "border"; "border-gray-300"; "focus:ring-blue-500"; "focus:border-blue-500"; "dark:bg-gray-700"; "dark:border-gray-600"; "dark:placeholder-gray-400"; "dark:text-white"; "dark:focus:ring-blue-500"; "dark:focus:border-blue-500"]
                         prop.key "Input"
                         prop.placeholder "Please enter some text"
                         prop.onChange (UpdateText >> dispatch)            
                     ]
-                ]
-            ]
-            Html.div [
-                prop.className ["flex flex-wrap justify-center gap-4"]
-                prop.children [
-                    Html.button [
-                        prop.className ["px-8"; "py-4"; "bg-gradient-to-r"; "from-blue-500"; "to-purple-500"; "text-white"; "font-bold"; "rounded-full"; "transition-transform"; "transform-gpu"; "hover:-translate-y-1"; "hover:shadow-lg"]
-                        prop.key "Submit"
-                        prop.onClick (fun _ -> dispatch Submit)
-                        prop.text "Send"
+                    Html.div [
+                            prop.key "ButtonContainer"
+                            prop.className ["flex"; "flex-wrap";"justify-end";"gap-4";"w-full";"mt-2"]
+                            prop.children [
+                                Html.button [
+                                    prop.name "SubmitButton"
+                                    prop.className ["cursor-pointer"; "select-none"; "px-8"; "py-4"; "bg-gradient-to-r"; "from-blue-500"; "to-purple-500"; "text-white"; "font-bold"; "rounded-full"; "transition-transform"; "transform-gpu"; "hover:-translate-y-1"; "hover:shadow-lg"]
+                                    prop.key "Submit"
+                                    prop.onClick (fun _ -> dispatch Submit)
+                                    prop.text "Send"
+                                    ]
+                                ]                
+                        ]                    
                     ]
-                ]                
             ]
             
-    
-
         ]
-
-    ]
-    
+    ]    
 Program.mkProgram init update render
 |> Program.withReactSynchronous "elmish-app"
 |> Program.run
